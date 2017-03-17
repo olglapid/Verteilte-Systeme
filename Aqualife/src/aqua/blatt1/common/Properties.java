af4a8babbf769e12566e6a35e81a49df37f2d559
Ola <olglapid@htwg-konstanz.de>
2017-03-17 09:22:00 +0100

    Initial commit

---
+++ Aqualife/src/aqua/blatt1/common/Properties.java
@@ -0,0 +1,7 @@
+package aqua.blatt1.common;
+
+public class Properties {
+	public final static String HOST = "localhost"; //141.37.121.31 Kevin
+	public final static int PORT = 4711;
+	public final static String BROKER_NAME = "AquaBroker";
+}
