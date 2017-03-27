b247c904b61727928f2e8e33a2bda2940a665fcf
olglapid <olalapidus@gmail.com>
2017-03-27 13:11:27 +0200

    Update Broker.java

--- Aqualife/src/aqua/blatt1/broker/Broker.java
+++ Aqualife/src/aqua/blatt1/broker/Broker.java
@@ -2,6 +2,9 @@ package aqua.blatt1.broker;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import aqua.blatt1.common.Direction;
 import aqua.blatt1.common.msgtypes.DeregisterRequest;
@@ -18,27 +21,34 @@ public class Broker {
 	static Endpoint endpoint = new Endpoint(4711);
 
 	static ClientCollection<InetSocketAddress> list = new ClientCollection<>();
+	static int NUM_THREADS = 5; // beispiel;
+	static ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+	static ReadWriteLock lock = new ReentrantReadWriteLock();
 
 	public static void broker() {
 
-		Message message = endpoint.blockingReceive();
-		BrokerTask newMessage = new BrokerTask();
-		newMessage.test(message);
+		while (true) {
+			Message message = endpoint.blockingReceive();
+			executor.execute(new BrokerTask(message));
+		}
 	}
 
 	public static void register(InetSocketAddress sender) {
 
+		lock.writeLock().lock();
 		String id = "Aquarium " + i;
 		i++;
 
 		list.add(id, sender);
 
 		endpoint.send(sender, new RegisterResponse(id));
+		lock.writeLock().unlock();
 
 	}
 
 	public static void handoff(InetSocketAddress sender, HandoffRequest handoff) {
-		
+
+		lock.readLock().lock();
 		InetSocketAddress temp;
 
 		int index = list.indexOf(sender);
@@ -48,13 +58,14 @@ public class Broker {
 			temp = list.getRightNeighorOf(index);
 		}
 		endpoint.send(temp, handoff);
+		lock.readLock().unlock();
 
 	}
 
 	public static void deregister(DeregisterRequest deregisterRequest) {
 
 		int index = list.indexOf(deregisterRequest.getId());
-		
+
 		if (index != -1) {
 			list.remove(index);
 		}
@@ -63,51 +74,40 @@ public class Broker {
 
 	public static void main(String args[]) {
 		broker();
+		executor.shutdown();
 	}
-	
-	public class BrokerTask  implements Runnable {
-		
-		
-		
-		public void test(Message message){
-			while (true) {
-	
-				if (message.getPayload() instanceof RegisterRequest) {
-	
-					register(message.getSender());
-	
-				} else if (message.getPayload() instanceof HandoffRequest) {
-	
-					handoff(message.getSender(), (HandoffRequest) message.getPayload());
-	
-				} else if (message.getPayload() instanceof DeregisterRequest) {
-	
-					deregister((DeregisterRequest) message.getPayload());
-	
-				}
-	
-			}
+
+	public static class BrokerTask implements Runnable {
+
+		private Message message;
+
+		public BrokerTask(Message message) {
+			this.message = message;
 		}
 
-		int NUM_THREADS = 5;	//beispiel;
+		public void test() {
+
+			if (message.getPayload() instanceof RegisterRequest) {
+
+				register(message.getSender());
+
+			} else if (message.getPayload() instanceof HandoffRequest) {
+
+				handoff(message.getSender(), (HandoffRequest) message.getPayload());
+
+			} else if (message.getPayload() instanceof DeregisterRequest) {
+
+				deregister((DeregisterRequest) message.getPayload());
+
+			}
+
+		}
+
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub		
+			test();
 		}
 		
-		Thread thread = new Thread(new BrokerTask());
-		thread.start();
-		
-		
-		ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
-//				while(true){
-//					executor.execute(new Runnable(){
-//						public void run(){
-//						// Implementierung der Task
-//						}
-//					});
-//				}
-//		executor.shutdown();
-		
+
 	}
 }
